c029634d1aa7ee949e8734142b93006d176f0f37
Slava Germonenko <slava.germonenko@gmail.com>
2022-05-11 20:28:55 +0500

    User model improvements.

--- src/WoodenWorkshop.Users.Core/Models/User.cs
+++ src/WoodenWorkshop.Users.Core/Models/User.cs
@@ -1,13 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 using WoodenWorkshop.Common.Core.Models;
 
 namespace WoodenWorkshop.Users.Core.Models;
 
 public class User : BaseModel
 {
+    [Required(ErrorMessage = "Имя – обязательное поле.")]
+    [StringLength(100, ErrorMessage = "Максимальная длина имени – 100 символов.")]
     public string FirstName { get; set; } = string.Empty;
 
+    [Required(ErrorMessage = "Фамилия – обязательное поле.")]
+    [StringLength(100, ErrorMessage = "Максимальная фамилии – 100 символов.")]
     public string LastName { get; set; } = string.Empty;
 
+    [Required(ErrorMessage = "Адрес электронной почты – обязательное поле.")]
+    [StringLength(250, ErrorMessage = "Максимальная длина адреса электронной почты – 250 символов.")]
     public string EmailAddress { get; set; } = string.Empty;
 
     public string PasswordHash { get; set; } = string.Empty;
